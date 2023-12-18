2b6172964343d71e67584df619578d46616885ae
JITHESH M <110782382+jitheshm@users.noreply.github.com>
2023-12-18 18:35:16 +0530

    sample (#1)
    * presentation added
    
    * Readme file added
    
    * Installation related bugs fixed
    
    * Update README.md
    
    * Installation script corrected
    
    * launcher updated
    
    * debian packaging files added
    
    * Snake and Ladder changed to Snakes and Ladders
    
    * Combobox filled with default values
    
    * Adjust default size dynamically based on screen dimensions
    
    * Closing Issue Fixed And Background music Added
    
    ---------
    
    Co-authored-by: Nalin-x-Linux <Nalin.x.Linux@gmail.com>
    Co-authored-by: VSarchana <archanavs1211@gmail.com>
    Co-authored-by: greeshnasarath <greeshnamohan001@gmail.com>

--- SnakesAndLadders/main.py
+++ SnakesAndLadders/main.py
@@ -22,36 +22,55 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ###########################################################################
 import gi 
+import webbrowser
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk 
-from board import GameBoard
+from SnakesAndLadders.board import GameBoard
+import pygame
 
 class SelectPlay(Gtk.Window):
 	def __init__(self):
 		Gtk.Window.__init__(self, title="Select Player")
+
+		pygame.mixer.init()
+
+		# Load and play the background music
+		pygame.mixer.music.load('/usr/share/SnakesAndLadders/sounds/bgmusic.ogg')
+		pygame.mixer.music.set_volume(0.2)
+		pygame.mixer.music.play(-1)
+
 		self.set_default_size(500, 500)
-		
-		
+		self.user_guide_file_path="https://www.google.com/"
 
 		#We will fix this later 
 		#self.connect("destroy", Gtk.main_quit)
 		#sets background colour for the window
-		self.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1, 0.7, 0.9, 1))
+		self.override_background_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(1, 1, 1, 1))
 		
+
 		#creates an alignment container and add to the window 
 		alignment = Gtk.Alignment()
-		alignment.set(0.5, 0.5, 0, 0.5)
+		alignment.set(0.5, 0.5, 0.5, 0)
 		self.add(alignment)
 		
 		#create vertical box container and add it to the alignment container
-		vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+		vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
 		alignment.add(vbox)
+		hbox1 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=100)
+		vbox.add(hbox1)
+		
+		hbox3 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=110)
+		vbox.add(hbox3)
+		hbox2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
+		vbox.add(hbox2)
+		hbox4 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=200)
+		hbox5 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
 		#creates a label and set its text and font color
 		label1 = Gtk.Label()
 		label1.set_text("Choose gameboard  :")
 		label1.set_use_underline(True)
 		label1.override_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.0, 0.0, 1.0, 1.0))
-		vbox.pack_start(label1, False, False, 10) # add label to the vertical box container
+		hbox1.pack_start(label1, False, False, 30) # add label to the vertical box container
 		
 		board_combo=Gtk.ComboBoxText()
 		board_combo.connect("changed", self.on_board_selected)
@@ -60,15 +79,28 @@ class SelectPlay(Gtk.Window):
 		board_combo.append_text("Board3")
 		board_combo.append_text("Board4")
 		board_combo.append_text("Board5")
-		board_combo.set_active(1)
+		board_combo.set_active(0)
 		label1.set_mnemonic_widget(board_combo)
-		vbox.pack_start(board_combo, False, False, 10)
+		hbox1.pack_start(board_combo, False, False, 20)
 		
+		label3 = Gtk.Label()
+		label3.set_text("Choose game mode  :")
+		label3.set_use_underline(True)
+		label3.override_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.0, 0.0, 1.0, 1.0))
+		hbox3.pack_start(label3, False, False, 30)
+		
+		game_mode_combo = Gtk.ComboBoxText()
+		game_mode_combo.connect("changed", self.on_game_mode_changed)
+		#adding values to combobox
+		game_mode_combo.append_text("Normal  mode")
+		game_mode_combo.append_text("Education mode")
+		hbox3.pack_start(game_mode_combo, False, False, 0)
+		label3.set_mnemonic_widget(game_mode_combo)
 		label2 = Gtk.Label()
 		label2.set_text("Choose the number of players:")
 		label2.set_use_underline(True)
 		label2.override_color(Gtk.StateFlags.NORMAL, Gdk.RGBA(0.0, 0.0, 1.0, 1.0))
-		vbox.pack_start(label2, False, False, 10)
+		hbox2.pack_start(label2, False, False, 30)
 		
 		player_count_combo = Gtk.ComboBoxText()
 		player_count_combo.connect("changed", self.on_player_count_changed)
@@ -82,20 +114,22 @@ class SelectPlay(Gtk.Window):
 
 		player_count_combo.set_size_request(100, 30)
 		#adding combobox to vbox
-		vbox.pack_start(player_count_combo, False, False, 0)
+		hbox2.pack_start(player_count_combo, False, False, 50)
 
 		#creating entry for adding player names
-		self.player_name_entries_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-		vbox.pack_start(self.player_name_entries_vbox, False, False, 20)
+		self.player_name_entries_vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+		hbox5.pack_start(self.player_name_entries_vbox, False, False, 170)
+		vbox.add(hbox5)
+		
 		
 		#creating start button
 		start_button = Gtk.Button(label="Start")
 		#connect the clicked signal to the on_submit_clicked method
 		start_button.connect("clicked", self.on_submit_clicked)
 		start_button.set_size_request(100, 30)
+		hbox4.pack_start(start_button, False, False, 200)
+		vbox.add(hbox4)
 		
-		vbox.pack_start(self.player_name_entries_vbox, False, False, 10)
-		vbox.pack_start(start_button, False, False, 10)
 		
 		#creating about button
 		about_button=Gtk.Button(label="About")
@@ -103,21 +137,29 @@ class SelectPlay(Gtk.Window):
 		about_button.set_size_request(20, 30)
 		
 		#creating help button
-		help_button=Gtk.Button(label="Help")
-		help_button.set_size_request(20, 30) 
+		user_button=Gtk.Button(label="User-Guide")
+		user_button.connect("clicked",self.on_user_clicked)
+		user_button.set_size_request(20, 30)
 		
-		submit_vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=50)
-		submit_vbox.pack_start(about_button, False, False, 0)
-		submit_vbox.pack_start(help_button, False, False, 0)
-		vbox.pack_start(submit_vbox, False, False, 50)
+		submit_vbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=0)
+		submit_vbox.pack_start(about_button, False, False, 100)
+		submit_vbox.pack_start(user_button, False, False, 0)
+		vbox.pack_start(submit_vbox, False, False, 80)
 		
 
-		vbox.pack_start(submit_vbox, False, False, 0)  
+		 
 		#empty list is created to hold player names
 		self.player_name_entries = []
 		self.set_resizable(False)
+
+		game_mode_combo.set_active(0)
+		player_count_combo.set_active(0)
+
 		self.show_all()
 		
+		self.connect("destroy", self.quit)
+
+		Gtk.main()
 		
 		
 	def on_player_count_changed(self, combo):
@@ -144,19 +186,32 @@ class SelectPlay(Gtk.Window):
 			self.player_name_entries_vbox.remove(entry)
 		self.player_name_entries.clear()
 		
+	def on_game_mode_changed(self,combo):
+		self.mode=combo.get_active()
+		
 	def on_board_selected(self,combo):
 		self.board_num = combo.get_active() + 1
 		
 	def on_submit_clicked(self, button):
 		#gets the text from each player name entry field and stores them in the player_names
 		player_names = [entry.get_text() for entry in self.player_name_entries]
-		#destroy the window
-		self.destroy()
+
+
 		#if all player names are provided, a GameBoard instance is created with the count and player names
 		if all(player_names):
-			game_board = GameBoard(self.board_num,self.count, player_names)
+			pygame.mixer.music.stop()
+			#destroy the window
+			self.destroy()
+
+			game_board = GameBoard(self.board_num,self.count, player_names,self.mode)
 			game_board.connect("destroy", Gtk.main_quit)
 			
+	def on_user_clicked(self,widget,data=None):
+		url = self.user_guide_file_path
+		try:
+			webbrowser.get("firefox").open(url, new=2)
+		except webbrowser.Error:
+			webbrowser.open(url, new=2)
 	def show_about_dialog(self):
 		about_dialog = Gtk.AboutDialog()
 
@@ -177,7 +232,7 @@ class SelectPlay(Gtk.Window):
 		
 	def on_about_clicked(self,button):
 		 self.show_about_dialog()
-    
-win = SelectPlay()
 
-Gtk.main()
+	def quit(self,widget, data=None):
+		self.destroy()
+		Gtk.main_quit()
